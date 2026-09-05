9209eacce70bbb5932d041526f43ab323e8454bc
Khrystyna <tkachristina079@gmail.com>
2026-09-05 19:51:00 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 from datetime import datetime
 from time import sleep
 
+
 def main() -> None:
     while True:
         current_time = datetime.now()
         file_name = (
-                f"app-{current_time.hour}_"
-                f"{current_time.minute}_"
-                f"{current_time.second}.log"
+            f"app-{current_time.hour}_"
+            f"{current_time.minute}_"
+            f"{current_time.second}.log"
         )
         with open(file_name, "w") as f:
             f.write(str(current_time))
